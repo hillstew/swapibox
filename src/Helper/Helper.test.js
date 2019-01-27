2ce346c146f73925580d4e89c7fe4986eb4ceb87
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-27 10:19:35 -0700

    Add getCleanPeople test

---
+++ src/Helper/Helper.test.js
@@ -0,0 +1,55 @@
+import * as helper from "./Helper";
+import { getCleanPeople } from "./Helper";
+
+describe("Helper", () => {
+  describe("getCleanPeople", () => {
+    let fetchData;
+    let mockSpecies;
+    let mockSpeciesFetch;
+    let mockHomeWorld;
+    let mockHomeWorldFetch;
+
+    beforeEach(async () => {
+      fetchData = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          json: () => ({ results: "array of 10 people" })
+        })
+      );
+      mockSpecies = {
+        name: "Human",
+        classification: "mammal",
+        designation: "sentient"
+      };
+      mockHomeWorld = {
+        name: "Tatooine",
+        rotation_period: "23",
+        orbital_period: "304",
+        diameter: "10465",
+        climate: "arid"
+      };
+      mockHomeWorldFetch = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve(mockHomeWorld));
+      mockSpeciesFetch = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve(mockSpecies));
+    });
+
+    it("should call fetchData with the persons homeworld as a param", () => {
+      //setup
+      let mockPeople = [
+        { name: "Luke Skywalker", height: "172", mass: "77" },
+        { name: "C-3PO", height: "167", mass: "75" }
+      ];
+      //execution
+      helper.getCleanPeople(mockPeople)
+      //expectation
+
+      expect(fetchData).toHaveBeenCalledWith(mockHomeWorld)
+    });
+
+    it.skip("should call fetchData with the persons species as a param", () => {});
+
+    it.skip("should call Promise.all with the correct params", () => {});
+  });
+});
