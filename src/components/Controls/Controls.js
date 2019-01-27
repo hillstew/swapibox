ffbc1ac5990b50f0a7fc0a4347581499bc8e14cc
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-27 15:40:21 -0700

    Add spans to controls buttons to rotate text

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -3,9 +3,15 @@ import React from "react";
 export const Controls = props => {
   return (
     <div className="controls-div">
-      <button onClick={props.handleChange} name="vehicles">VEHICLES</button>
-      <button onClick={props.handleChange} name="planets">PLANETS</button>
-      <button onClick={props.handleChange} name="people">PEOPLE</button>
+      <button onClick={props.handleChange} name="vehicles">
+        <span>VEHICLES</span>
+      </button>
+      <button onClick={props.handleChange} name="planets">
+        <span>PLANETS</span>
+      </button>
+      <button onClick={props.handleChange} name="people">
+        <span>PEOPLE</span>
+      </button>
     </div>
   );
 };
