83d37eb8913e960d0ba6598352ebd5b7f030b148
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-10 22:21:14 -0700

    Add mapStateToProps to Card Container

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,21 +1,30 @@
-import React from "react";
-import Card from "../Card/Card";
-import PropTypes from "prop-types";
+import React, { Component } from "react"
+import Card from "../Card/Card"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
 
-const CardContainer = props => {
-  let cards = props[props.view].map(subject => {
-    return (
-      <Card subject={subject} view={props.view} key={subject.name} />
-    );
-  });
-  return <div>{cards}</div>;
-};
+export class CardContainer extends Component {
+  render() {
+    return this.props[this.props.view].map((subject) => {
+      return (
+        <Card subject={subject} view={this.props.view} key={subject.name} />
+      )
+    })
+  }
+}
 
 CardContainer.propTypes = {
   view: PropTypes.string,
   people: PropTypes.array,
   planets: PropTypes.array,
   vehicles: PropTypes.array
-};
+}
 
-export default CardContainer;
+const mapStateToProps = (state) => ({
+  planets: state.planets,
+  vehicles: state.vehicles,
+  people: state.people,
+  view: state.view
+})
+
+export default connect(mapStateToProps)(CardContainer)
